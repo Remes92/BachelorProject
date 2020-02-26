2ef8f080f9597135a7077138c1899cfe0e891903
Remes92 <joakimremes@hotmail.com>
2020-02-26 14:01:11 +0100

    Skapat modeller

---
+++ BachelorProjectBackend/Models/Company.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BachelorProjectBackend.Models
+{
+    public class Company
+    {
+        public int CompanyId { get; set; }
+        public string Company_Name { get; set; }
+        public bool Company_DirectionIn { get; set; }
+        public bool Company_DirectionOut { get; set; }
+        public string Company_Phone { get; set; }
+        public string Company_Label { get; set; }
+        public string Company_Slogan { get; set; }
+    }
+}
---
+++ BachelorProjectBackend/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BachelorProjectBackend.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public int CompanyId { get; set; }
+        public string Department_Description { get; set; }
+        public string Department_Key { get; set; }
+        public string Department_Label { get; set; }
+        public string Department_Name { get; set; }
+    }
+}
